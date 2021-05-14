6164ff4def383b0264b8d90e554dd1696c155d97
jachoo <1050784+jachoo@users.noreply.github.com>
2021-05-14 19:06:25 +0200

    Workarounds for few init errors (numpy, sip). Also nice splash screen added.

--- cvlab/__init__.py
+++ cvlab/__init__.py
@@ -1,31 +1,63 @@
 import os
+import sys
 
 from .version import __version__
 
 CVLAB_DIR = os.path.abspath(__file__ + "/..").replace("\\", "/")
 
 
-def main(*args, **kwargs):
-    import sys
-    import numpy as np
+def init_libs():
+    # system exception hook
     try:
-        import sip
-    except ImportError:
-        from PyQt5 import sip
+        sys._excepthook = sys.excepthook
+        def exception_hook(exctype, value, traceback):
+            sys._excepthook(exctype, value, traceback)
+            sys.exit(-1)
+        sys.excepthook = exception_hook
+    except Exception:
+        pass
 
-    sys._excepthook = sys.excepthook
+    # numpy error exceptions
+    try:
+        import numpy as np
+        np.seterr(all='raise')
+    except Exception:
+        pass
 
-    def exception_hook(exctype, value, traceback):
-        sys._excepthook(exctype, value, traceback)
-        sys.exit(-1)
+    # PyQt exceptions workaround
+    try:
+        try:
+            from PyQt5 import sip
+        except ImportError:
+            import sip
+        sip.setdestroyonexit(False)
+    except Exception:
+        pass
 
-    sys.excepthook = exception_hook
 
-    np.seterr(all='raise')
-    sip.setdestroyonexit(False)
+def splash():
+    from PyQt5.QtGui import QPixmap
+    from PyQt5.QtWidgets import QSplashScreen, QLabel, QDesktopWidget
+    from PyQt5.QtCore import QTimer
+
+    pixmap = QPixmap(CVLAB_DIR + "/images/splash.png")
+    splash = QSplashScreen(pixmap)
+
+    splash.show()
+    splash.raise_()
+
+    QTimer.singleShot(5000, splash.close)
+
+    return splash
+
+
+def main(*args, **kwargs):
+    init_libs()
 
     from PyQt5.QtWidgets import QApplication
     app = QApplication(sys.argv)
+
+    s = splash()
 
     from .view.mainwindow import MainWindow
     main_window = MainWindow(app)
@@ -34,4 +66,4 @@ def main(*args, **kwargs):
 
 
 if __name__ == '__main__':
-    main()
+    exit(main())
